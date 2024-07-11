0914244bd0f6a7029bbe5262533ed9acd1c86ee9
gmorange <gmorange@student.42.fr>
2024-07-11 16:34:47 +0200

    OLL OK

--- ThirdLayer.py
+++ ThirdLayer.py
@@ -90,43 +90,176 @@ def resolve_yellow_cross():
         cube.print_cube()
         binary_face = set_binary_face('Y', C5, A11, C4, A10, A6, C7, A8, C6)
 
+def convert_moves(moves: list) -> list:
+    actions = []
+
+    for m in moves:
+        if m[0] == 'R':
+            actions.append((0, 1)[len(m) == 2])
+        elif m[0] == 'L':
+            actions.append((2, 3)[len(m) == 2])
+        elif m[0] == 'F':
+            actions.append((4, 5)[len(m) == 2])
+        elif m[0] == 'B':
+            actions.append((6, 7)[len(m) == 2])
+        elif m[0] == 'U':
+            actions.append((8, 9)[len(m) == 2])
+        elif m[0] == 'D':
+            actions.append((10, 11)[len(m) == 2])
+    return actions
+
 def first_step_two_look_oll():
+    print("first step")
+    '''Make a cross on yellow face'''
     binary_face = set_binary_face()
     instructions = []
     cube.print_cube()
     if (binary_face & 0b00000100011000100000) == 0b00000100011000100000:
         print("CROSS")
         pass
-    elif binary_face & ((1<<1) | (1<<8) | (1<<11) | (1<<18)) == ((1<<1) | (1<<8) | (1<<11) | (1<<18)):
+    elif binary_face & 0b01000000100100000010 == 0b01000000100100000010:
         print("point")
-        instructions = [4, 2, 10, 3, 11, 5, 6, 10, 0, 11, 1, 7]# F L D L' D' F' B D R D' R' B'
-    elif binary_face & ((1<<1) | (1<<9) | (1<<10) | (1<<18)) == ((1<<1) | (1<<9) | (1<<10) | (1<<18)):
+        instructions = convert_moves(["F", "L", "D", "L'", "D'", "F'", "B", "D", "R", "D'", "R'", "B'"])
+    elif binary_face & 0b01000000011000000010 == 0b01000000011000000010:
         print("normal line")
-        instructions = [4, 2, 10, 3, 11, 5]# F L D L' D' F'
-    elif binary_face & ((1<<5) | (1<<8) | (1<<11) | (1<<14)) == ((1<<5) | (1<<8) | (1<<11) | (1<<14)):
+        instructions = convert_moves(["F", "L", "D", "L'", "D'", "F'"])
+    elif binary_face & 0b00000100100100100000 == 0b00000100100100100000:
         print("vertical line")
-        instructions = [10, 4, 2, 10, 3, 11, 5]# D F L D L' D' F'
+        instructions = convert_moves(["D", "F", "L", "D", "L'", "D'", "F'"])
     elif binary_face &  0b01000000101000100000 == 0b01000000101000100000:
         print("normal L")
-        instructions = [6, 10, 0, 11, 1, 7] # B D R D' R' B' 
+        instructions = convert_moves(["B", "D", "R", "D'", "R'", "B'"]) 
     elif binary_face &  0b01000000010100100000 == 0b01000000010100100000:
         print("down left L")
-        instructions = [11, 6, 10, 0, 11, 1, 7] # D' B D R D' R' B' 
+        instructions = convert_moves(["D'", "B", "D", "R", "D'", "R'", "B'"]) 
     elif (binary_face & 0b00000100010100000010) == 0b00000100010100000010:
         print("up right L")
-        instructions = [10, 10, 6, 10, 0, 11, 1, 7] # DD  B D R D' R' B' 
+        instructions = convert_moves(["D", "D", "B", "D", "R", "D'", "R'", "B'"]) 
     elif (binary_face & 0b00000100101000000010) == 0b00000100101000000010:
         print("up left L")
-        instructions = [10, 6, 10, 0, 11, 1, 7] # D B D R D' R' B' 
+        instructions = convert_moves(["D", "B", "D", "R", "D'", "R'", "B'"]) 
     for i in instructions:
         action(i)()
     cube.print_cube()
 
 
+def second_step_two_look_oll():
+    print("second_step")
+    cube.print_cube()
+    binary_face = set_binary_face()
+    instructions = []
+    #fig 2
+    # L D2 L' D' L' D' L'
+
+    #fig 3
+    # L D2 L2 D' L2 D' L2 D2 L
+    print(bin(binary_face))
+    #FIG 8
+    if (binary_face & 0b00001110011001110000) == 0b00001110011001110000:
+        print("Yellow face ok");
+        pass
+
+    #FIG 1
+    elif (binary_face & 0b10000101011001100001) == 0b10000101011001100001:
+        print("Fig 1")
+        instructions = convert_moves(["L", "D", "L'", "D", "L", "D", "D", "L'"])
+    elif (binary_face & 0b10000101011010110000) == 0b10000101011010110000:
+        print("Fig 1 with D")
+        instructions = convert_moves(["D", "L", "D", "L'", "D", "L", "D", "D", "L'"])
+    elif (binary_face & 0b1101011010100001) == 0b1101011010100001:
+        print("Fig 1 with D'")
+        instructions = convert_moves(["D'", "L", "D", "L'", "D", "L", "D", "D", "L'"])
+    elif (binary_face & 0b10000110011010100001) == 0b10000110011010100001:
+        print("Fig 1 with D D")
+        instructions = convert_moves(["D", "D", "L", "D", "L'", "D", "L", "D", "D", "L'"])
+
+    #FIG 2
+    elif (binary_face & 0b10110011000101100) == 0b10110011000101100:
+        print("Fig 2")
+        instructions = convert_moves(["L", "D", "D", "L'", "D'", "L", "D'", "L'"])
+    elif (binary_face & 0b101100011000101100) == 0b101100011000101100:
+        print("Fig 2 with D")
+        instructions = convert_moves(["D", "L", "D", "D", "L'", "D'", "L", "D'", "L'"])
+    elif (binary_face & 0b110100011000110100) == 0b110100011000110100:
+        print("Fig 2 with D'")
+        instructions = convert_moves(["D'", "L", "D", "D", "L'", "D'", "L", "D'", "L'"])
+    elif (binary_face & 0b110100011001101000) == 0b110100011001101000:
+        print("Fig 2 with D D")
+        instructions = convert_moves(["D", "D", "L", "D", "D", "L'", "D'", "L", "D'", "L'"])
+
+    #FIG 3
+    elif (binary_face & 0b110100011010100001) == 0b110100011010100001:
+        print("Fig 3")
+        instructions = convert_moves(["L", "D", "D", "L", "L", "D'", "L", "L", "D'", "L", "L", "D", "D", "L"])
+    elif (binary_face & 0b10101011000100101) == 0b10101011000100101:
+        print("Fig 3 with D")
+        instructions = convert_moves(["D", "L", "D", "D", "L", "L", "D'", "L", "L", "D'", "L", "L", "D", "D", "L"])
+    elif (binary_face & 0b10100100011010101000) == 0b10100100011010101000:
+        print("Fig 3 with D'")
+        instructions = convert_moves(["D'", "L", "D", "D", "L", "L", "D'", "L", "L", "D'", "L", "L", "D", "D", "L"])
+    elif (binary_face & 0b10000101011000101100) == 0b10000101011000101100:
+        print("Fig 3 with D D")
+        instructions = convert_moves(["D", "D", "L", "D", "D", "L", "L", "D'", "L", "L", "D'", "L", "L", "D", "D", "L"])
+
+    #FIG 4
+    elif (binary_face & 0b10100100011000100101) == 0b10100100011000100101:
+        print("Fig 4")
+        instructions = convert_moves(["F", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "F'"])
+    elif (binary_face & 0b00010101011010101000) == 0b00010101011010101000:
+        print("Fig 4 with D")
+        instructions = convert_moves(["D", "F", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "L", "D", "L'", "D'", "F'"])
+
+    #FIG 5
+    elif(binary_face & 0b10000110011000110100) == 0b10000110011000110100:
+        print("Fig 5")
+        instructions = convert_moves(["R", "F", "L'", "F'", "R'", "F", "L", "F'"])
+    elif(binary_face & 0b00001110011010101000) == 0b00001110011010101000:
+        print("Fig 5 with D")
+        instructions = convert_moves(["D", "R", "F", "L'", "F'", "R'", "F", "L", "F'"])
+    elif(binary_face & 0b00010101011001110000) == 0b00010101011001110000:
+        print("Fig 5 with D'")
+        instructions = convert_moves(["D'", "R", "F", "L'", "F'", "R'", "F", "L", "F'"])
+    elif(binary_face & 0b00101100011001100001) == 0b00101100011001100001:
+        print("Fig 5 with D2")
+        instructions = convert_moves(["D", "D", "R", "F", "L'", "F'", "R'", "F", "L", "F'"])
+
+    #FIG 6
+    elif(binary_face & 0b10110011001100001) == 0b10110011001100001:
+        print("Fig 6")
+        instructions = convert_moves(["F'", "R", "F", "L'", "F'", "R'", "F", "L"])
+    elif(binary_face & 0b1101011000110100) == 0b1101011000110100:
+        print("Fig 6 with D")
+        instructions = convert_moves(["D", "F'", "R", "F", "L'", "F'", "R'", "F", "L"])
+    elif(binary_face & 0b101100011010110000) == 0b101100011010110000:
+        print("Fig 6 with D'")
+        instructions = convert_moves(["D'", "F'", "R", "F", "L'", "F'", "R'", "F", "L"])
+    elif(binary_face & 0b10000110011001101000) == 0b10000110011001101000:
+        print("Fig 6 with D D")
+        instructions = convert_moves(["D", "D", "F'", "R", "F", "L'", "F'", "R'", "F", "L"])
+
+    #FIG 7
+    elif (binary_face & 0b00001110011000100101) == 0b00001110011000100101:
+        print("Fig 7")
+        instructions = convert_moves(["L", "L", "U", "L'", "D", "D", "L", "U'", "L'", "D", "D", "L'"])
+    elif (binary_face & 0b00010110011010110000) == 0b00010110011010110000:
+        print("Fig 7 with D'")
+        instructions = convert_moves(["D'", "L", "L", "U", "L'", "D", "D", "L", "U'", "L'", "D", "D", "L'"])
+    elif (binary_face & 0b00001101011001101000) == 0b00001101011001101000:
+        print("Fig 7 with D")
+        instructions = convert_moves(["D", "L", "L", "U", "L'", "D", "D", "L", "U'", "L'", "D", "D", "L'"])
+    elif (binary_face & 0b10100100011001110000) == 0b10100100011001110000:
+        print("Fig 7 with D2")
+        instructions = convert_moves(["D", "D", "L", "L", "U", "L'", "D", "D", "L", "U'", "L'", "D", "D", "L'"])
+
+    for i in instructions:
+        action(i)()
+    cube.print_cube()
+
 def resolve_yellow_face():
-    
+    print("")
     # try with python3 main.py "F2 D2 L' F2 L D2 F D' F"
     first_step_two_look_oll()
+    second_step_two_look_oll()
 #    resolve_yellow_cross();
 #       yellow_faces_list = [C5, A11, C4, A10, A6, C7, A8, C6]
 #       binary_yellow = 0
